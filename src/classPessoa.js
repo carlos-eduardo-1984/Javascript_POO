16e61a6fb500f50683b23ee79986c10c5218a8da
Senior Tester - SQA Carlos Oliveira <159614680+carlos-eduardo-1984@users.noreply.github.com>
2024-02-10 10:52:23 +0000

    Add files via upload

---
+++ src/classPessoa.js
@@ -0,0 +1,13 @@
+class Pessoa { 
+
+    constructor(primeiroNome, sbrNome) {  
+
+		this.nome = primeiroNome; 
+		this.sobrenome = sbrNome; } //Atributos
+
+	exibir(dado) { console.log(dado) } //Metodo
+
+} 
+
+module.exports = Pessoa
+
